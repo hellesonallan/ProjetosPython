c75075c872ca27e21f6c28a49a168e0ae27962d2
hellesonallan <hellesonallan20@gmail.com>
2024-06-25 16:36:43 -0300

    adicionado gerador de senhas

---
+++ gerador-senhas/app.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+# Função de pergunta
+def pergunta(prompt):
+    resposta = input(prompt).strip().upper()
+    while resposta not in ["S", "N", "SIM", "NAO", "NÃO"]:
+        resposta = input(prompt).strip().upper()
+    return resposta
+
+def gerar_senha():
+    # Definir variáveis
+    tamanho = int(input("Quantos caracteres? (0/128): "))
+    opcoes = []
+
+    # Perguntar ao usuário sobre as opções
+    if pergunta("Incluir letras? (S/N): ") in ["S", "SIM"]:
+        opcoes.extend(string.ascii_letters)
+    if pergunta("Incluir números? (S/N): ") in ["S", "SIM"]:
+        opcoes.extend(string.digits)
+    if pergunta("Incluir símbolos e letras especiais? (S/N): ") in ["S", "SIM"]:
+        opcoes.extend(string.punctuation)
+    if pergunta("Incluir espaços em branco? (S/N): ") in ["S", "SIM"]:
+        opcoes.extend(string.whitespace)
+
+    # Gerar a senha
+    senha = str().join(random.choices(opcoes, k=tamanho))
+    return senha
+
+print("Senha Gerada:", gerar_senha())
